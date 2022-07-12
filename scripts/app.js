fe334f9e57ccf17b2239ed30e3fca17c58ff282d
Jimmy Smith <jimmy.a.smith@gmail.com>
2022-07-11 18:03:28 -0700

    UsersV1

---
+++ scripts/app.js
@@ -0,0 +1,8 @@
+//create the user object constructor
+
+
+//create the register function
+
+    //get the value from the inputs from the form
+    //create the object
+    //display the user in the console 
